b2c4186c2984a659f5105fe2941949f521615d84
Pierre <pierrematar1912@gmail.com>
2025-03-07 20:32:35 +0100

    chore : add base dependancies

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-slate-900 text-4xl">
+    hello
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@pinia/nuxt',
+    '@nuxtjs/google-fonts',
+  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  future: {
+    compatibilityVersion: 4
+  },
+
+  css: ['@/assets/css/main.css'],
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 })
