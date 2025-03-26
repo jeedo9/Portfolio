0055ff328fd2faf14d281ee7cba886393db217ba
Pierre <pierrematar1912@gmail.com>
2025-03-26 22:44:57 +0100

    refact : remove unused html attribute

--- app/components/ui/logos/LogoMain.vue
+++ app/components/ui/logos/LogoMain.vue
@@ -17,5 +17,5 @@ import { AUTHOR_NAME, DEFAULT_DELAY } from '~/utils/constants';
    
 </script>
 <template>
-        <as role="button" :class="twMerge(`cursor-grab active:cursor-grabbing relative before:content-[''] before:absolute before:h-px before:w-0 before:bg-accent before:right-0 before:bottom-1 hover:before:w-[105%] hover:before:right-auto hover:before:left-0 before:transition-all opacity-0 ${animation}`, className)" :style="{animationDelay: delay}" autocapitalize="words" >{{ name }}</as>
+        <as role="button" :class="twMerge(`cursor-grab active:cursor-grabbing relative before:content-[''] before:absolute before:h-px before:w-0 before:bg-accent before:right-0 before:bottom-1 hover:before:w-[105%] hover:before:right-auto hover:before:left-0 before:transition-all opacity-0 ${animation}`, className)" :style="{animationDelay: delay}" >{{ name }}</as>
 </template>
