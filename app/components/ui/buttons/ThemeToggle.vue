8457af763733dab1906b8699c147878805b2c656
Pierre <pierrematar1912@gmail.com>
2025-03-26 22:43:55 +0100

    feat : add dark mode

--- app/components/ui/buttons/ThemeToggle.vue
+++ app/components/ui/buttons/ThemeToggle.vue
@@ -11,12 +11,14 @@ const props = defineProps<ThemeToggleProps>()
 
 const {className = '', type = 'button', ...rest} = props
 
+const modeValue = computed(() => useColorMode().value)
 
 </script>
 
 <template>
-          <button v-bind="rest"  :type :class="twMerge(` size-7 flex justify-center items-center rounded-md bg-muted/22 hover:bg-muted/40 transition-[background-color]`, className)">
-            <Icon aria-hidden="false" aria-label="dark mode" name="mynaui:moon-star-solid" />
+          <button @click="modeValue === 'light' ? useColorMode().preference = 'dark' : useColorMode().preference = 'light'" v-bind="rest"   :type :class="twMerge(` size-7 flex justify-center items-center rounded-md bg-muted/22 hover:bg-muted/40 transition-[background-color]`, className)">
+            <Icon  v-bind="modeValue === 'dark' ? {hidden: true} : {} "  aria-hidden="false" :aria-label="modeValue + ' mode'" name="mynaui:moon-star-solid" />
+            <Icon  v-bind="modeValue === 'dark' ? {} :  {hidden: true} "  aria-hidden="false" :aria-label="modeValue + ' mode'" name="mynaui:sun" />
          </button>
 
 </template>
